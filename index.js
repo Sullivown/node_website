d88b134e03ae93af89df5527a467440ee933d4a0
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-09-21 11:56:14 +0100

    updated port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
 	let pageData;
